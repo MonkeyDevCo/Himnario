a48b0b98aed3fb8094ee45d20b7f0c763b2b841a
Sergio Flores <serflo.elec@gmail.com>
2020-02-18 00:00:32 -0300

    template base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PoliticsComponent } from './politics/politics.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'politics', component: PoliticsComponent },
+	{ path: 'faq', component: FaqComponent },
+    { path: '**',  redirectTo: '/home' },
+    { path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PoliticsComponent } from './politics/politics.component';
+import { FaqComponent } from './faq/faq.component';
+
+import {MatExpansionModule} from '@angular/material/expansion';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PoliticsComponent,
+    FaqComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
